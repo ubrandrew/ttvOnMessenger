eacb1844b743217cb3202df7efb25ad56ae8c6aa
ubrandrew <uberandrew126@gmail.com>
2020-05-18 00:56:27 -0400

    adding build script to streamline build process

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Building popup and merging with chrome extension 
+
+cd ttvmotes
+yarn build 
+
+cd ..
+rm -rf MessengerMotesBuild
+rm -rf MessengerMotesBuild
+
+
+mkdir MessengerMotesBuild
+cp -a ttvmotes/build/. MessengerMotesBuild
+cp -a MessengerMotes/. MEssengerMotesBuild
